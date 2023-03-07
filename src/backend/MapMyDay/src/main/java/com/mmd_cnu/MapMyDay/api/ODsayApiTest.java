6754edc93bb11e81bcb52b60b99433dd19fed6a6
Ham3798 <43927226+Ham3798@users.noreply.github.com>
2023-03-07 19:58:53 +0900

    ODsay API test

---
+++ src/backend/MapMyDay/src/main/java/com/mmd_cnu/MapMyDay/api/ODsayApiTest.java
@@ -0,0 +1,37 @@
+package com.mmd_cnu.MapMyDay.api;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+public class ODsayApiTest {
+    public static void main(String[] args) throws IOException {
+
+        // ODsay Api Key 정보
+        String apiKey = "4zVzlDJLvu3atqAt+eZOeEGXHYifZ9AUVo7A+/Ck0/s";
+
+        String urlInfo = "https://api.odsay.com/v1/api/searchPubTransPathT?SX=126.9027279&SY=37.5349277&EX=126.9145430&EY=37.5499421&apiKey=" + URLEncoder.encode(apiKey, "UTF-8");
+
+        // http 연결
+        URL url = new URL(urlInfo);
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+
+        BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line);
+        }
+        bufferedReader.close();
+        conn.disconnect();
+
+        // 결과 출력
+        System.out.println(sb.toString());
+
+    }
+}
